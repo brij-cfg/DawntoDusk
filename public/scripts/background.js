f4698e0c28c8faaf0d863b395d64dbbec2e24423
Brij <brijpatel94@gmail.com>
2018-03-10 21:38:11 -0600

    github Desktop

---
+++ public/scripts/background.js
@@ -0,0 +1,15 @@
+
+var counter = 0;
+function changeBG(){
+    var imgs = [
+        "url(https://images.unsplash.com/photo-1465695954255-a262b0f57b40?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=06d92f647a2937af54f658e199c3d990&auto=format&fit=crop&w=1050&q=80)",
+      ]
+    
+    if(counter === imgs.length) counter = 0;
+    $("body").css("background-image", imgs[counter]);
+
+    counter++;
+}
+  
+  setInterval(changeBG, 2000);
+
